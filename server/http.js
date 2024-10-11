5ac6524fb51ebcb3767ddc0fa2eec5cdd54909f2
RafaelGSS <rafael.nunu@hotmail.com>
2024-10-11 10:39:44 -0300

    fixup! feat: add http native /plain

--- server/http.js
+++ server/http.js
@@ -8,5 +8,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(process.env.PORT, '0.0.0.0', () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://0.0.0.0:${port}/`);
 });
