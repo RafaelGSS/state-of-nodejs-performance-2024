917a685889c02780bd31d87541fa190165b86e98
RafaelGSS <rafael.nunu@hotmail.com>
2024-10-11 10:36:59 -0300

    feat: add http native /plain

---
+++ server/http.js
@@ -0,0 +1,12 @@
+const http = require('node:http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  res.end('Hello, World!\n');
+});
+
+server.listen(process.env.PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
